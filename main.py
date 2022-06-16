3a9c70325dd25f5958e4f076c3f358a12242fed4
apollyon1993 <45370819+apollyon1993@users.noreply.github.com>
2022-06-16 20:26:18 -0300

    Adicionar modelo do vosk

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-# Our main file.
+# our main file.
 
-import speech_recognition as sr
+from vosk import Model, KaldiRecognizer
+import os
 import pyaudio
 
+model - Model("model")
+rec = KaldiRecognizer(model, 16000)
 
-# Criar um reconhecedor
-r = sr.Recognizer()
+p = pyaudio.pyaudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
 
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
+    else:
+        print(rec.PartialResult())
 
-
-#Abrir microfone para captura
-
-with sr.Microphone() as source:
-    while True:
-        audio = r.listen(source) # Define microfone como fonte de áudio
-        
-        print(r.recognize_google(audio, language='pt'))
+print(rec.FinalResult())
