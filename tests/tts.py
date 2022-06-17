250541d315aa7c634cae94f0848aca7d0e6fde0a
apollyon1993 <45370819+apollyon1993@users.noreply.github.com>
2022-06-16 23:31:52 -0300

    Adicionando talk back
    Falta investigar o ovewfloewd to OS

--- tests/tts.py
+++ tests/tts.py
@@ -1,4 +1,8 @@
 import pyttsx3
 engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-0].id)
+
 engine.say("Agora sim esta indo tudo numa boa")
 engine.runAndWait()
